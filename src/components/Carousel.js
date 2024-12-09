9b7db6ef748cab77fdc7b5a67de2150921f76e67
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2024-12-08 21:50:27 -0800

    home changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,42 @@
-import React from "react";
-import '../styles/Carousel.css';
+import React, { useState, useEffect } from "react";
+import "../styles/Carousel.css";
 import Briana_ad from "../images/Briana_ad.jpg";
 import Suhani_ad from "../images/Suhani_ad.jpg";
 import Parnika_ad from "../images/Parnika_ad.jpg";
 
-function Caraousel() {
+function Carousel() {
     const images = [Briana_ad, Suhani_ad, Parnika_ad];
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [exitingIndex, setExitingIndex] = useState(null);
 
-    const goToPrevious = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length -1 : prevIndex -1));
-    };
-
-    const goToNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex ===images.length -1 ? 0 : prevIndex + 1));
-
-    };
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setExitingIndex(currentIndex);
+            setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        }, 5000); // Change every 5 seconds
+        return () => clearInterval(interval);
+    }, [currentIndex]);
 
     return (
-        <div class="carousel">
-            <div className="left-arrow" onClick ={goToPrevious}>
-                <p>Prev</p>
-            </div>
+        <div className="carousel">
             <div className="carousel-items">
-                <img src = {images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className = "carousel-image" />
-            </div>
-            <div className="right-arrow" onClick={goToNext}>
-                <p>Next</p>
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt={`Slide ${index + 1}`}
+                        className={`carousel-image ${
+                            index === currentIndex
+                                ? "active"
+                                : index === exitingIndex
+                                ? "exiting"
+                                : ""
+                        }`}
+                    />
+                ))}
             </div>
         </div>
     );
 }
 
-export default Caraousel;
+export default Carousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,29 @@
 // src/pages/About.js
 import React from "react";
-import Banner from "../components/Banner";
-import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
+import '../styles/Home.css';
 
 function Home() {
     return (
-        <div>
+        <div className="home">
+            {/* Carousel Section */}
             <Carousel />
+            <hr className="horizontal-line" />
+            
+            {/* Home Content Section */}
+            <div className="home-content">
+                <h1>Welcome to Student Hustle</h1>
+                <p>
+                    Student Hustle is a service designed for college students to:
+                </p>
+                <ul>
+                    <li>Operate side hustles efficiently</li>
+                    <li>Discover verified lifestyle services on campus</li>
+                    <li>Enhance a sense of community on campus</li>
+                </ul>
+            </div>
         </div>
     );
 }
 
-export default Home; // Default export
+export default Home;
